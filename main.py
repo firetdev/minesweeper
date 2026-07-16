2c1755519a59af876d575941f1245cd91647e819
firetdev <futuregamesstore@gmail.com>
2026-07-15 23:44:40 -0500

    Added game functionality

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import random
 
-grid = []
-mines = set()
+def print_grid(grid):
+    for row in grid:
+        for cell in row:
+            if cell == -1:
+                print("M", end=" ")
+            else:
+                print(cell, end=" ")
+        print()
 
-for i in range(10):
-    grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid = [[0 for _ in range(10)] for _ in range(10)]
+mines = set()
 
 while len(mines) < 10:
     mines.add((random.randint(0, 9), random.randint(0, 9)))
@@ -33,10 +39,31 @@ for x, row in enumerate(grid):
                 
         grid[x][y] = count
 
-for row in grid:
-    for cell in row:
-        if cell == -1:
-            print("M", end=" ")
+game_grid = [["#" for _ in range(10)] for _ in range(10)]
+game_over = False
+
+while not game_over:
+    print_grid(game_grid)
+    flag = input("Do you want to flag a cell? (y/n): ").lower()
+    if flag == "y":
+        x = int(input("Enter row (0-9): "))
+        y = int(input("Enter column (0-9): "))
+        if game_grid[x][y] == "#":
+            game_grid[x][y] = "F"
+        elif game_grid[x][y] == "F":
+            game_grid[x][y] = "#"
+    else:
+        x = int(input("Enter row (0-9): "))
+        y = int(input("Enter column (0-9): "))
+
+        if grid[x][y] == -1:
+            print("Game Over! You hit a mine.")
+            game_over = True
+            print_grid(grid)
         else:
-            print(cell, end=" ")
-    print()
+            game_grid[x][y] = grid[x][y]
+            if all(game_grid[i][j] != "#" for i in range(10) for j in range(10) if grid[i][j] != -1):
+                print("Congratulations! You've cleared the minefield.")
+                game_over = True
+
+print_grid(grid)
