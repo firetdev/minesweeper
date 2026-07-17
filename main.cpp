e1c0e7936d287e4e41bf4f62d054d195fe35e9a4
firetdev <futuregamesstore@gmail.com>
2026-07-17 14:24:39 -0500

    Removed comment

--- main.cpp
+++ main.cpp
@@ -103,7 +103,6 @@ int main() {
             } else {
                 gameGrid[x][y] = grid[x][y];
                 
-                // Wins check
                 if ([&]() {
                     for (int i = 0; i < 10; ++i) {
                         for (int j = 0; j < 10; ++j) {
